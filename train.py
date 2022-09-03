175a2db6ec5fcac927faa345facaa1e24ba39169
Omar <omar.tarek29@msa.edu.eg>
2022-09-03 22:55:36 +0200

    finalizing

--- train.py
+++ train.py
@@ -13,6 +13,68 @@ def CreateTemplateFromVideo(videoPath, label,target_lms):
     lms = GetLandMarksFromVideo(videoPath,target_lms)
     return Template(label, lms)
 
+def GetLandMarksFromFrames(frames,target_lms,activites_number):
+    mp_drawing = mp.solutions.drawing_utils
+
+    mp_pose = mp.solutions.pose
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    # org
+
+    # fontScale
+    fontScale = 1
+
+    # Blue color in BGR
+    color = (0, 0, 0)
+
+    # Line thickness of 2 px
+    thickness = 2
+    landmarks = []
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        for image in frames:
+
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+            results = pose.process(image)
+            if results.pose_landmarks == None:
+                continue
+
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                      mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                      )
+            lm = results.pose_landmarks.landmark
+
+            for target in target_lms:
+                landmarks.append(Point(lm[target].x, lm[target].y))
+                image = cv2.putText(image, 'shots: ' + str(activites_number[0]), (20,20), font,
+                                    fontScale, color, thickness, cv2.LINE_AA)
+                image = cv2.putText(image, 'juggles: ' + str(activites_number[1]), (20, 50), font,
+                                    fontScale, color, thickness, cv2.LINE_AA)
+                cv2.imshow('Mediapipe Feed', image)
+
+                if cv2.waitKey(10) & 0xFF == ord('q'):
+                    break
+    return landmarks
+
+
+
+def GetFramesOfVideo(path):
+    cap = cv2.VideoCapture(path)
+    frames = []
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
+
+    print ("FRAMES COUNT OF TEST :" ,frames)
+    return frames
+
 
 def GetLandMarksFromVideo(videoPath,target_lms):
     mp_drawing = mp.solutions.drawing_utils
@@ -20,6 +82,11 @@ def GetLandMarksFromVideo(videoPath,target_lms):
     mp_pose = mp.solutions.pose
 
     cap = cv2.VideoCapture(videoPath)
+
+
+
+
+
 
     landmarks = []
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
@@ -61,6 +128,11 @@ def GetLandMarksFromVideo(videoPath,target_lms):
             for target in target_lms:
                 landmarks.append(Point(lm[target].x,lm[target].y))
 
+            cv2.imshow('Mediapipe Feed', image)
+
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
 
     return landmarks
 
@@ -68,6 +140,16 @@ def GetLandMarksFromVideo(videoPath,target_lms):
 #
 # print (GetLandMarksFromVideo("VIDEOS/juggle/Juggle_1.mp4"))
 
+
+def getactivityduration(path):
+    cap = cv2.VideoCapture(path)
+    fps = cap.get(cv2.CAP_PROP_FPS)      # OpenCV2 version 2 used "CV_CAP_PROP_FPS"
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    duration = frame_count/fps
+    seconds = int(duration%60)
+    print(seconds)
+    return seconds
+    cap.release()
 
 
 
@@ -77,30 +159,69 @@ def GetLandMarksFromVideo(videoPath,target_lms):
 
 
 directories = [
-    "shoot"
+    "shoot","juggle"
 ]
 
 results = []
+
+i=0
 videosPaths =os.listdir("TINYVIDEOS/" + "shoot")
+print(i, "/", len(videosPaths))
+testPath = videosPaths[i]
+testPath = os.path.join("TINYVIDEOS/", "shoot", testPath)
+templates = []
+targetLandmarks = [25,26]
 
-for i in range(0, len(videosPaths)):
-    print(i, "/", len(videosPaths))
-    testPath = videosPaths[i]
-    testPath = os.path.join("TINYVIDEOS/", "shoot", testPath)
-    templates = []
+for directory in directories:
+    for idx, filename in enumerate(videosPaths):
+        if idx == i:
+            continue
+        path = os.path.join("TINYVIDEOS/", directory, filename)
+        print (directory, " " , filename)
+        # print (GetLandMarksFromVideo(path))
+        getactivityduration(path)
+        templates.append(CreateTemplateFromVideo(path, directory, targetLandmarks))
 
-    for directory in directories:
-        for idx, filename in enumerate(videosPaths):
-            if idx == i:
-                continue
-            path = os.path.join("TINYVIDEOS/", directory, filename)
-            # print (GetLandMarksFromVideo(path))
-            templates.append(CreateTemplateFromVideo(path, directory,[15+10,10+10,4+10,6+10]))
+recognizer = Recognizer(templates)
 
-    recognizer = Recognizer(templates)
+print("testing for shoot")
 
-    print("testing for shoot")
 
-    result = recognizer.recognize(GetLandMarksFromVideo(testPath, [15+10,10+10,4+10,6+10]))
+duration = getactivityduration("test_videos/shoot.mp4")
+framesOfVideo = GetFramesOfVideo("test_videos/shoot.mp4")
+
+print (len(framesOfVideo))
+fps = int (len(framesOfVideo)/duration)
+
+print ("fps are " , fps)
+
+
+
+
+# result = recognizer.recognize(GetLandMarksFromVideo(testPath, range(10,33)))
+# print(result)
+# results.append(result)
+
+
+#testing
+
+step = fps * 1
+start = 0
+shoot =0
+juggle = 0
+while start < len(framesOfVideo)-step:
+    end = start + step
+    result = recognizer.recognize(GetLandMarksFromFrames(framesOfVideo[start:end], targetLandmarks,[shoot,juggle]))
     print(result)
-    results.append(result)
+    if (result[0] == 'shoot'): shoot+=1
+    if (result[0] == 'juggle'): juggle+=2
+    start += step
+
+
+print ("shoots taken :" ,shoot)
+print ("juggles done :" ,juggle)
+
+
+
+
+
